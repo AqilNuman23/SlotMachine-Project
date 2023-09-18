cef7d64f3e455155f2640c1a259d0297bf98b2ff
Aqil Nu'man <130033381+AqilNuman23@users.noreply.github.com>
2023-09-18 12:39:13 +0800

    Update index.js
    Till Step 4 Finish

--- index.js
+++ index.js
@@ -7,9 +7,24 @@
 // 7. Play again
 const prompt = require("prompt-sync")();
 
+//  all import and libraries -> global variables -> classses and fucntion -> main lines or other aspect of the code
+// any global var practice must using capital letter
+const ROWS = 3;
+const COLS = 3;
 
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
 
-
+const SYMBOLS_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
 
 const deposit = () => {
     while (true) {
@@ -54,7 +69,30 @@ const getBet = (balance, lines) => {
     }
 };
 
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i=0; i<count; i++) {
+            symbols.push(symbol);
+        }
+    }
 
+    const reels = [[], [], []];
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j = 0; j <ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            // adding and remove used symbol
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
+};
+
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const numberBet = getBet(balance, numberOfLines);
